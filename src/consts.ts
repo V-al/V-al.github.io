0e6fbe82d0aa6d73feb7f840c3d2315b14cc3187
Val <124918918+Vaaal0@users.noreply.github.com>
2026-05-05 21:15:34 -0600

    init: blog No soy cyberpunk

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'No soy Cyberpunk';
+export const SITE_DESCRIPTION = 'Un blog sobre cultura, estética y filosofía cyberpunk.';
