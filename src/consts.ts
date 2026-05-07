4e78d7b02bd5a6e2005b17e678d8d8a09321f724
Val <124918918+Vaaal0@users.noreply.github.com>
2026-05-06 20:00:27 -0600

    Ghost y actualización de nombre

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'No soy Cyberpunk';
+export const SITE_TITLE = '反乱';
 export const SITE_DESCRIPTION = 'Un blog sobre cultura, estética y filosofía cyberpunk.';
